140beb1f19dc189d77941371c562387891939171
timydaley <tdaley@stanford.edu>
2019-05-12 19:42:27 -0700

    add max pooling

--- models/conv_model.py
+++ models/conv_model.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 from torch.utils import data
 import sys
 import pandas
+import math
 from dataLoader import OrganoidDataset
 
 
@@ -13,23 +14,27 @@ from dataLoader import OrganoidDataset
 
 # Convolutional neural network (two convolutional layers)
 class SimpleConvNet(nn.Module):
-  def __init__(self, in_channels = 1, layer1channels = 16, layer2channels = 16, out_size = 1):
+  def __init__(self, in_channels = 1, layer1channels = 16, layer2channels = 16, out_size = 1, in_size = 193):
     super(SimpleConvNet, self).__init__()
     self.layer1 = nn.Sequential(nn.Conv2d(in_channels, layer1channels,
                                           kernel_size=5, stride=1, padding=2),
                                 nn.BatchNorm2d(layer1channels),
-                                nn.ReLU())
+                                nn.ReLU(),
+                                nn.MaxPool2d(2))
+    layer1dim = math.floor((in_size -1 - 1)/2) + 1
     self.layer2 = nn.Sequential(nn.Conv2d(layer1channels, layer2channels,
                                           kernel_size=5, stride=1, padding=2),
                                 nn.BatchNorm2d(layer2channels),
-                                nn.ReLU())
+                                nn.ReLU(),
+                                nn.MaxPool2d(2))
+    layer2dim = math.floor((layer1dim -1 - 1)/2) + 1
     self.layer3 = nn.Sequential(nn.Conv2d(layer2channels, layer2channels,
                                           kernel_size=5, stride=1, padding=2),
                                 nn.BatchNorm2d(layer2channels),
                                 nn.ReLU(),
-                                nn.MaxPool2d(2),
-                                )
-    self.fc = nn.Linear(in_features = 193*193*layer2channels, out_features = out_size)
+                                nn.MaxPool2d(2))
+    layer3dim = math.floor((layer2dim -1 - 1)/2) + 1
+    self.fc = nn.Linear(in_features = layer3dim*layer3dim*layer2channels, out_features = out_size)
   def forward(self, x):
     out = self.layer1(x)
     out = self.layer2(out)
