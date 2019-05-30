830b1ac4691882dbfd7ab748a853a8e01d5a82cd
Timothy Patrick Daley <tdaley@sh-08-35.int>
2019-05-29 18:48:57 -0700

    update data loader with experiment option

--- models/dataLoader.py
+++ models/dataLoader.py
@@ -9,28 +9,35 @@ import math
 
 class OrganoidDataset(data.Dataset):
   'dataset class for microwell organoid images'
-  def __init__(self, path2files, well_labels, day_label_X, Y, intensity_mean = 0.5, intensity_var = 0.025):
-    assert len(well_labels) == len(Y)
-    assert len(day_label_X) == len(Y)
+  def __init__(self, path2files, experiments, image_names, Y, mean_sd_dict):
+    assert len(image_names) == len(Y)
+    assert len(experiments) == len(image_names)
     self.path = path2files
-    #self.mw_labels = microwell_labels
-    self.well_labels = well_labels
-    self.day_label_X = day_label_X
+    self.experiments = experiments
+    self.image_names = image_names
     self.Y = Y
-    self.mean = intensity_mean
-    self.sd = math.sqrt(intensity_var)
+    self.mean_sd_dict = mean_sd_dict
   def __len__(self):
     return len(self.Y)
   def getXimage(self, index):
-    img_name = 'well' + str(self.well_labels[index]) + '_day' + str(self.day_label_X[index]) + '_well.png'
-    img_loc = os.path.join(self.path, img_name)
-    # skimage.io.imread returns a numpy array
+    img_name = self.image_names[index]
+    experiment = self.experiments[index]
+    img_loc = str(experiment) + '/' + img_name
+    img_loc = os.path.join(self.path, img_loc)
     image = io.imread(img_loc)
-    # convert to grey scale
-    image = np.true_divide(color.rgb2gray(image) - self.mean, self.sd)
-    # add color axis because torch image: CxHxW
-    image = np.reshape(image, newshape = (1, image.shape[0], image.shape[1]))
+    mean, sd = self.mean_sd_dict[day]
+    image = np.true_divide(color.rgb2gray(image) - mean, sd)
     return torch.from_numpy(image).float()
+#  def getXimage(self, index):
+#    img_name = 'well' + str(self.well_labels[index]) + '_day' + str(self.day_label_X[index]) + '_well.png'
+#    img_loc = os.path.join(self.path, img_name)
+    # skimage.io.imread returns a numpy array
+#    image = io.imread(img_loc)
+    # convert to grey scale
+#    image = np.true_divide(color.rgb2gray(image) - self.mean, self.sd)
+    # add color axis because torch image: CxHxW
+#    image = np.reshape(image, newshape = (1, image.shape[0], image.shape[1]))
+#    return torch.from_numpy(image).float()
   def getY(self, index):
     Y = self.Y[index]
     return torch.from_numpy(np.asarray(self.Y[index], dtype=float)).float()
